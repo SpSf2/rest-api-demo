bcddf41489217277441abcf7b18ca5caca18a426
SpSf2 <kiworks.duglas@gmail.com>
2026-09-21 12:30:10 +0200

    Solución del @DisplayName modificando el pom al final

--- src/test/java/com/example/RestApiDemoApplicationTests.java
+++ src/test/java/com/example/RestApiDemoApplicationTests.java
@@ -1,9 +1,11 @@
 package com.example;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@DisplayName ("Test de Carga del Contexto de Spring Boot")
 class RestApiDemoApplicationTests {
 
 	@Test  // Este Test es para verificar que el contexto de Spring se levanta correctamente, 
